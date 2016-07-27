3287d10a667332686dc7985a620c09254b63b6e4
HP <krist.ng@corous360.com>
2016-07-27 11:43:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/questdot/splashscreenexample/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.questdot.splashscreenexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends Activity {
+    private ProgressBar spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        spinner = (ProgressBar)findViewById(R.id.progressBar);
+
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, 2000);
+    }
+}
